a9b11ba44eb343bbb211bb8d8f4f9f84ad951c45
benivalotker <valotkerbeni@gmail.com>
2019-02-22 14:55:33 +0200

    app.js declare

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express'), 
+app = express(),
+db = require('./models/db');
+
+const bodyParser = require('body-parser'),
+cors = require('cors'),
+config = require('./configuration/config');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+//enable cors
+app.use(cors());
+
+app.use(require('./controllers'))
+
+db.connect(config.db.connection.uri, (err) => {
+   if(err){
+        console.log('Unable to connect to MongoDB.')
+        process.exit(1)
+   }else{
+        app.listen(config.server.port, config.server.host, () => {
+            console.log(`app running on http://${config.server.host}:${config.server.port}`);
+        });
+   }
+});
+
+
+
