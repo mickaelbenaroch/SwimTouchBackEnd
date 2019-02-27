5b02dc302eca84bbaded02121a25ae75a9b6dfbe
benivalotker <valotkerbeni@gmail.com>
2019-02-27 21:00:09 +0200

    corss

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ app.use(bodyParser.json({limit: '50mb'}));
 
 //enable cors
 //CORS problem solved
-app.use((req, res, next) => { res.header("Access-Control-Allow-Origin", "*"); 
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
+/*app.use((req, res, next) => { res.header("Access-Control-Allow-Origin", "*"); 
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });*/
+app.use(cors());
 
 app.use(require('./controllers'))
 
