8427d6f8648038bdcf771a0ef7d50ecd0006ee29
benivalotker <valotkerbeni@gmail.com>
2019-03-25 08:18:31 +0200

    get statistic page

--- controllers/statistic.js
+++ controllers/statistic.js
@@ -2,9 +2,24 @@
 
 const express = require('express'),
 route = express.Router();
+let http = require('http');
+let fs = require('fs');
+const path = require("path");
+
+
 
 //get user profile
-route.post('/', (req, res)=>{
+route.get('/', (req, res)=>{
+    fs.readFile(path.resolve(__dirname, "../public/statistic/d3.html"), null, function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('Whoops! File not found!');
+        } else {
+            console.log("innnnn")
+            res.write(data);
+        }
+        res.end();
+    });
 });
 
 
