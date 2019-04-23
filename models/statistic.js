d16f89d73fdbcc218572297896f0cca4785c773d
benivalotker <valotkerbeni@gmail.com>
2019-04-23 17:45:52 +0300

    get full records

--- models/statistic.js
+++ models/statistic.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var db = require('./db'); 
+var db = require('./db');  
 
 //get all swimmer records
 exports.getStatisticByswimmer = (swimmer_ref) => {
@@ -43,32 +43,37 @@ exports.getStatisticByDate = (obj_records) => {
     });
 }
 
-//get full swimmer records (with exercise 7 swimer details)
+//get full swimmer records by name (with exercise & swimer details)
 exports.getFullStatistic = (obj_records) => {
-    let db_table = db.get().collection('st-record');
+    const db_record = db.get().collection('st-record');
+    const db_exercise = db.get().collection('st-exercise');
+    const db_swimmer = db.get().collection('st-swimmer');
 
     return new Promise(( res, rej) => {
-       try {
-            if(obj_records.end_date == undefined)
-                throw "end date is empty"
+        //find all swimmer records
+        db_record.find({'swimmer.swimmer_ref': obj_records }).toArray((err, result) =>{
+            if(err || result === undefined || result.length == 0)
+                rej("error to get records")
+            else
+                res(result);
+        });  
+    }).then(result => {
+        //then get swimmer profile
+        return new Promise(( res, rej) => {
+            db_swimmer.findOne({'_id': result[0].swimmer.swimmer_id }, (err, data) =>{
+                if(err || data === undefined || data === null || data.length == 0)
+                    return "error to get records" 
+            
+                result.forEach((element, i) => {
+                    element.swimmer = data
+                });
 
-            db_table.find({date: {$gte: obj_records.start_date, $lte: obj_records.end_date},  'swimmer.swimmer_ref': obj_records.swimmer_ref }).toArray((err, result) =>{
-                if(err || result === undefined || result.length == 0)
-                    rej("error to get records")
-                else
-                    res(result);
-            });   
-       } catch (error) {
-            db_table.find({date: obj_records.start_date,  'swimmer.swimmer_ref': obj_records.swimmer_ref}).toArray((err, result) =>{
-                if(err || result === undefined || result.length == 0)
-                    rej("error to get records")
-                else
-                    res(result);
+                res(result)
             });
-       }
-    });
+        });
+
+    }).then((result, ex )=> {
+        //then get swimmer exercise
+        return result
+    }); 
 }
-
-
-
-
