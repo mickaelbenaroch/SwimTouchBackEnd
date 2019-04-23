cd6a6fde4165fb2efec2b63ad84c643c313d3ba6
benivalotker <valotkerbeni@gmail.com>
2019-04-23 11:53:35 +0300

    secure validate - get data records function

--- controllers/statistic.js
+++ controllers/statistic.js
@@ -6,7 +6,7 @@ records = require('../models/statistic'),
 { check, validationResult } = require('express-validator/check');
 
 //get all swimmer records (swimmer is require)
-route.get('/swimmer', check('swimmer_ref').not().isEmpty() ,(req, res)=>{
+route.get('/swimmer_record', check('swimmer_ref').not().isEmpty() ,(req, res)=>{
 
     records.getStatisticByswimmer(req.body.swimmer_ref).then((data) => {
         res.status(200).json({isTrue: true, records: data});   
@@ -19,13 +19,19 @@ route.get('/swimmer', check('swimmer_ref').not().isEmpty() ,(req, res)=>{
 });
 
 //get user records by date (start_date & swimmer name is require, end_date is optional)
-route.get('/date_swimmer', check('start_date').not().isEmpty(), check('swimmer_ref').not().isEmpty() ,(req, res)=>{
+route.get('/date_record', check('start_date').not().isEmpty(), check('swimmer_ref').not().isEmpty() ,(req, res)=>{
 
     var obj_records = JSON.parse(JSON.stringify({
         start_date: req.body.start_date,
         end_date: req.body.end_date,
         swimmer_ref: req.body.swimmer_ref
     }));
+
+    const errors = validationResult(req);
+    let validate_array = errors.array();
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: `${validate_array[0].param} is require` });
+    }
 
     records.getStatisticByDate(obj_records).then((data) => {
         res.status(200).json({isTrue: true, records: data});   
@@ -38,6 +44,16 @@ route.get('/date_swimmer', check('start_date').not().isEmpty(), check('swimmer_r
 });
 
 
-
+//get full swimmer records (with exercise 7 swimer details) (swimmer is require)
+route.get('/full_records', check('swimmer_ref').not().isEmpty() ,(req, res)=>{
+    records.getFullStatistic(req.body.swimmer_ref).then((data) => {
+        res.status(200).json({isTrue: true, records: data});   
+        res.end(); 
+    }).catch(err => {
+        res.json({isTrue: false, error: err})
+        res.status(500)
+        res.end()
+    })
+});
 
 module.exports = route
--- models/statistic.js
+++ models/statistic.js
@@ -43,5 +43,32 @@ exports.getStatisticByDate = (obj_records) => {
     });
 }
 
+//get full swimmer records (with exercise 7 swimer details)
+exports.getFullStatistic = (obj_records) => {
+    let db_table = db.get().collection('st-record');
+
+    return new Promise(( res, rej) => {
+       try {
+            if(obj_records.end_date == undefined)
+                throw "end date is empty"
+
+            db_table.find({date: {$gte: obj_records.start_date, $lte: obj_records.end_date},  'swimmer.swimmer_ref': obj_records.swimmer_ref }).toArray((err, result) =>{
+                if(err || result === undefined || result.length == 0)
+                    rej("error to get records")
+                else
+                    res(result);
+            });   
+       } catch (error) {
+            db_table.find({date: obj_records.start_date,  'swimmer.swimmer_ref': obj_records.swimmer_ref}).toArray((err, result) =>{
+                if(err || result === undefined || result.length == 0)
+                    rej("error to get records")
+                else
+                    res(result);
+            });
+       }
+    });
+}
+
+
 
 
