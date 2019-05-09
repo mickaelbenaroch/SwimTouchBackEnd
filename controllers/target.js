27cd8113977e094df995874e80020162df06fb08
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-05-09 11:06:43 +0300

    added team taget endpoints

--- controllers/target.js
+++ controllers/target.js
@@ -5,7 +5,7 @@ route = express.Router(),
 swimmer = require('../models/target'),
 uuidv4 = require('uuid/v4');
 
-//create new swimmer
+//create new swimmer target
 route.post('/swimmertarget', (req, res)=>{
     var obj_swimmer_target = {
         _id:            uuidv4(),
@@ -35,6 +35,45 @@ route.post('/getswimmertarget', (req, res)=>{
     }));
 
     swimmer.getSwimmerTarget(obj_swimmer_target).then((data) => {
+        res.status(200).json({isTrue: true, target: data});   
+        res.end(); 
+    }).catch(err => {
+        res.json({isTrue: false, error: err})
+        res.status(500)
+        res.end()
+    })
+});
+
+//create new team target
+route.post('/teamtarget', (req, res)=>{
+    var obj_team_target = {
+        _id:            uuidv4(),
+        distance:       req.body.distance, 
+        target:         req.body.targetTime,
+        triesToImprove: req.body.triesToImprove,
+        style:          req.body.style,
+        targetTime:     req.body.targetTime,
+        team_id:        req.body.team_ref,
+        date:           req.body.date
+   };
+
+    swimmer.createTeamTarget(obj_team_target).then((data) => {
+        res.status(200).json({isTrue: true, target_response: data});   
+        res.end(); 
+    }).catch(err => {
+        res.json({isTrue: false, error: err})
+        res.status(500)
+        res.end()
+    })
+});
+
+//regular get team target (filter)
+route.post('/getteamtarget', (req, res)=>{
+    var obj_team_target = JSON.parse(JSON.stringify({
+            team_id:    req.body.team_id
+    }));
+        
+    swimmer.getTeamTarget(obj_team_target).then((data) => {
         res.status(200).json({isTrue: true, target: data});   
         res.end(); 
     }).catch(err => {
--- models/target.js
+++ models/target.js
@@ -19,7 +19,7 @@ exports.createSwimmerTarget = (obj_swimmer_target) => {
     });
 }
 
-//regular get swimmer (filter)
+//regular get team target (filter)
 exports.getSwimmerTarget = (obj_swimmer_target) => {
     return new Promise(( res, rej) => {
         let swimmertarget = db.get().collection('st-swimmer-target');
@@ -36,3 +36,36 @@ exports.getSwimmerTarget = (obj_swimmer_target) => {
     });
 }
 
+//create new team target
+exports.createTeamTarget = (obj_team_target) => {
+    return new Promise(( res, rej) => {
+        let teamtarget = db.get().collection('st-team-target');
+        
+        teamtarget.insertOne(obj_team_target, (err, result) => {
+            if(err)
+                throw err;
+            else{
+               res(obj_team_target._id)
+            }
+        });
+    }).catch(error => {
+        rej("create new swimmer target failed")
+    });
+}
+
+//regular get swimmer (filter)
+exports.getTeamTarget = (obj_team_target) => {
+    return new Promise(( res, rej) => {
+        let teamtarget = db.get().collection('st-team-target');
+
+        teamtarget.find(obj_team_target).toArray((err, result) =>{
+            if(err || result === undefined || result.length == 0)
+                rej("error to get swimmers target")
+            else
+                res(result);
+        });
+        
+    }).catch(error => {
+        rej("error to get swimmers target")
+    });
+}
