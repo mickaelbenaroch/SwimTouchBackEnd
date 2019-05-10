c77df2545a00489602c2daf1ec029c594b9f3d63
benivalotker <valotkerbeni@gmail.com>
2019-05-10 12:37:51 +0300

    validate target

--- controllers/target.js
+++ controllers/target.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const express = require('express'),
+const { check } = require('express-validator/check'), 
+express = require('express'),
 route = express.Router(),
 swimmer = require('../models/target'),
-uuidv4 = require('uuid/v4');
+uuidv4 = require('uuid/v4'),
+valid_class = require('../controllers/API/validate'),
+log         = require('../controllers/API/logger');
 
-//create new swimmer target
+//Details - create new swimmer target
+//require - none
+//return  - boolean, true/false
 route.post('/swimmertarget', (req, res)=>{
     var obj_swimmer_target = {
         _id:            uuidv4(),
@@ -30,23 +35,33 @@ route.post('/swimmertarget', (req, res)=>{
     })
 });
 
-//regular get swimmer (filter)
-route.post('/getswimmertarget', (req, res)=>{
+//Details - get swimmer target
+//require - swimmer_ref
+//return  - swimmer target
+route.post('/getswimmertarget', check('swimmer_ref').not().isEmpty(), (req, res)=>{
     var obj_swimmer_target = JSON.parse(JSON.stringify({
             swimmer_ref:    req.body.swimmer_ref
     }));
 
-    swimmer.getSwimmerTarget(obj_swimmer_target).then((data) => {
-        res.status(200).json({isTrue: true, target: data});   
-        res.end(); 
-    }).catch(err => {
-        res.json({isTrue: false, error: err})
-        res.status(500)
-        res.end()
-    })
+    let validat_result = valid_class.valid_chack(req);
+
+    if(validat_result.next().value == false){
+        res.status(422).json({ errors: valid_class.error_valid(validat_result.next().value[0].param) });
+    }else{
+        swimmer.getSwimmerTarget(obj_swimmer_target).then((data) => {
+            res.status(200).json({isTrue: true, target: data});   
+            res.end(); 
+        }).catch(err => {
+            res.json({isTrue: false, error: err})
+            res.status(500)
+            res.end()
+        })
+    }
 });
 
-//create new team target
+//Details - create new team target
+//require - none
+//return  - boolean, true/false
 route.post('/teamtarget', (req, res)=>{
     var obj_team_target = {
         _id:            uuidv4(),
@@ -71,57 +86,79 @@ route.post('/teamtarget', (req, res)=>{
     })
 });
 
-//regular get team target (filter)
-route.post('/getteamtarget', (req, res)=>{
+//Details - get team target
+//require - team_id
+//return  - team target
+route.post('/getteamtarget', check('team_id').not().isEmpty(), (req, res)=>{
     var obj_team_target = JSON.parse(JSON.stringify({
             team_id:    req.body.team_id
     }));
         
-    swimmer.getTeamTarget(obj_team_target).then((data) => {
-        res.status(200).json({isTrue: true, target: data});   
-        res.end(); 
-    }).catch(err => {
-        res.json({isTrue: false, error: err})
-        res.status(500)
-        res.end()
-    })
+    let validat_result = valid_class.valid_chack(req);
+
+    if(validat_result.next().value == false){
+        res.status(422).json({ errors: valid_class.error_valid(validat_result.next().value[0].param) });
+    }else{
+        swimmer.getTeamTarget(obj_team_target).then((data) => {
+            res.status(200).json({isTrue: true, target: data});   
+            res.end(); 
+        }).catch(err => {
+            res.json({isTrue: false, error: err})
+            res.status(500)
+            res.end()
+        })
+    }
 });
 
-//regular get team target (filter)
-route.post('/updateswimmertarget', (req, res)=>{
+//Details - update swimmer target
+//require - _id (swimmer id)
+//return  - boolean,  true/false
+route.post('/updateswimmertarget', check('_id').not().isEmpty(), (req, res)=>{
     var obj_team_target = JSON.parse(JSON.stringify({
-            _id:  req.body._id,
-            done:         req.body.done,
+            _id:                        req.body._id,
+            done:                       req.body.done,
+            notification_has_been_send: req.body.notification_has_been_send
+    }));
+    let validat_result = valid_class.valid_chack(req);
+
+    if(validat_result.next().value == false){
+        res.status(422).json({ errors: valid_class.error_valid(validat_result.next().value[0].param) });
+    }else{
+        swimmer.updateSwimmerTarget(obj_team_target).then((data) => {
+            res.status(200).json({isTrue: true, target: data});   
+            res.end(); 
+        }).catch(err => {
+            res.json({isTrue: false, error: err})
+            res.status(500)
+            res.end()
+        })
+    }
+});
+
+//Details - update team target
+//require - _id (team id)
+//return  - boolean,  true/false
+route.post('/updateteamtarget', check('_id').not().isEmpty(), (req, res)=>{
+    var obj_team_target = JSON.parse(JSON.stringify({
+            _id:                        req.body._id,
+            done:                       req.body.done,
             notification_has_been_send: req.body.notification_has_been_send
     }));
         
-    swimmer.updateSwimmerTarget(obj_team_target).then((data) => {
-        res.status(200).json({isTrue: true, target: data});   
-        res.end(); 
-    }).catch(err => {
-        res.json({isTrue: false, error: err})
-        res.status(500)
-        res.end()
-    })
-});
+    let validat_result = valid_class.valid_chack(req);
 
-
-//regular get team target (filter)
-route.post('/updateteamtarget', (req, res)=>{
-    var obj_team_target = JSON.parse(JSON.stringify({
-            _id:    req.body._id,
-            done:         req.body.done,
-            notification_has_been_send: req.body.notification_has_been_send
-    }));
-        
-    swimmer.updateTeamTarget(obj_team_target).then((data) => {
-        res.status(200).json({isTrue: true, target: data});   
-        res.end(); 
-    }).catch(err => {
-        res.json({isTrue: false, error: err})
-        res.status(500)
-        res.end()
-    })
+    if(validat_result.next().value == false){
+        res.status(422).json({ errors: valid_class.error_valid(validat_result.next().value[0].param) });
+    }else{
+        swimmer.updateTeamTarget(obj_team_target).then((data) => {
+            res.status(200).json({isTrue: true, target: data});   
+            res.end(); 
+        }).catch(err => {
+            res.json({isTrue: false, error: err})
+            res.status(500)
+            res.end()
+        })
+    }
 });
 
 
